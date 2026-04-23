8fb80b842d4c5b8631a8bc1e67728347817bd8c3
Byeongsu Kim <byeongsukim@Edena.local>
2026-04-23 15:53:10 +0900

    Keep local helper files out of the public repo

--- DevTools/test_gesture.swift
+++
@@ -1,8 +0,0 @@
-import AppKit
-
-class MyDelegate: NSObject, NSGestureRecognizerDelegate {
-    func gestureRecognizer(_ gestureRecognizer: NSGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: NSGestureRecognizer) -> Bool {
-        return true
-    }
-}
-print("Compile success!")
